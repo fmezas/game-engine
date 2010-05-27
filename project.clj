21481f8b4f54ea1d36173b56a92f4b8265b46df5
Francisco Meza <pancho@sneezy.(none)>
2010-05-27 02:56:10 -0700

    removed text added by mistake

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tanks "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"](ClassLoader/getSystemResource "/images/fire.gif")
+  :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
