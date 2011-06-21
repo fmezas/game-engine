00fa8181c52c479215933a7268006070ca692c91
Francisco Meza <fmezas@gmail.com>
2011-06-21 01:38:25 -0700

    added project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tanks "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "basic game engine in clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [midje "1.1-alpha-1"]]
