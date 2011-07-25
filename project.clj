980442b556b871f91f74708ecb7423aa1cda7b48
Francisco Meza <fmezas@gmail.com>
2011-07-25 18:07:08 -0500

    verifying interaction with github

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tanks "1.0.0-SNAPSHOT"
-  :description "basic game engine in clojure"
+  :description "basic game engine in clojure with sample game"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [midje "1.1-alpha-1"]]
