b5e292aa698dedc73a96a952017076b7c4bfaff4
Francisco Meza <fmezas@gmail.com>
2013-02-01 15:17:26 -0800

    clojure upgrade; some .gitignore changes

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject tanks "1.0.0-SNAPSHOT"
   :description "basic game engine in clojure with sample game"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [midje "1.1-alpha-1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :plugins [[lein-swank "1.4.3"]])
